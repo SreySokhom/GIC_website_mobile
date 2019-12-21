85e1c27fab34979925c9b971cf185a3ecb8b8b7b
srey sokhom <41099990+SreySokhom@users.noreply.github.com>
2019-12-21 14:45:13 +0700

    Add project
    This is the start point of our project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GIC App',
+      home: HomeScreen()
+    )
+  );
+}
+  
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello world :)'),
+      ),
+    );
+  }
+}
