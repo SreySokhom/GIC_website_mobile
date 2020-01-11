777bd1b83ab228f3060f36a38806d7c9a6674150
CHEA-Menghong <cheamenghong123@gmail.com>
2020-01-11 08:43:58 +0700

    add scholarshipschool

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
+
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'GIC App',
-      home: HomeScreen()
-    )
-  );
-}
-  
-class HomeScreen extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hello world :)'),
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
       ),
     );
   }
